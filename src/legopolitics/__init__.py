008f2c9e84d66228a594d48e5b397ac2b76c4b0e
cantayus <cantay.caliskan@gmail.com>
2026-07-12 21:41:55 +0300

    Delete src/legopolitics directory

--- src/legopolitics/__init__.py
+++
@@ -1,6 +0,0 @@
-from legopolitics.analyzer import LegoPoliticsAnalyzer
-from legopolitics.config import AnalysisConfig
-from legopolitics.schemas import AnalysisResult
-from legopolitics.version import __version__
-
-__all__ = ["AnalysisConfig", "AnalysisResult", "LegoPoliticsAnalyzer", "__version__"]
