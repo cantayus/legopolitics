66a954e4ade3ae5a4aae84441c19c4a2380ee273
cantayus <cantay.caliskan@gmail.com>
2026-07-13 13:38:30 +0300

    Add files via upload

---
+++ src/legopolitics/constants.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+DISCLAIMER = (
+    "LEGO® is a trademark of the LEGO Group of companies, which does not sponsor, "
+    "authorize, or endorse this project. legopolitics is an independent academic "
+    "research software project and is not affiliated with, maintained by, sponsored "
+    "by, authorized by, or associated with the LEGO Group."
+)
+CONFIG_SCHEMA_VERSION = "1.0"
+DATABASE_SCHEMA_VERSION = "1.0"
+PUBLIC_RESULT_SCHEMA_VERSION = "1.0"
+SUPPORTED_VIDEO_EXTENSIONS = {".mp4", ".mkv", ".mov", ".avi", ".webm", ".m4v"}
+EXCEL_SHEETS = [
+    "README",
+    "Video",
+    "Scenes",
+    "Frames",
+    "Detections",
+    "Fused_Detections",
+    "Tracks",
+    "Track_Observations",
+    "Attributes",
+    "Relations",
+    "OCR",
+    "Transcript",
+    "Diarization",
+    "Audio_Events",
+    "Events",
+    "Narrative_Segments",
+    "Codebook_Scores",
+    "Model_Agreement",
+    "Validation",
+    "Run_Metadata",
+    "Prompts",
+    "Models",
+    "Errors",
+    "Data_Dictionary",
+]
