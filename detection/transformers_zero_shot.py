ae8c61e7609753afa6abdfdcc47ee373decdfc47
cantayus <cantay.caliskan@gmail.com>
2026-07-13 13:55:02 +0300

    Delete detection directory

--- detection/transformers_zero_shot.py
+++
@@ -1,5 +0,0 @@
-from legopolitics.detection.grounding_dino import GroundingDinoDetector
-
-
-class TransformersZeroShotDetector(GroundingDinoDetector):
-    """Generic Transformers zero-shot detector; model_id controls the backend model."""
