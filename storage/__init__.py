c973dcdd0d191e523802eccd015b08c7e2c924cb
cantayus <cantay.caliskan@gmail.com>
2026-07-13 13:56:24 +0300

    Delete storage directory

--- storage/__init__.py
+++
@@ -1,5 +0,0 @@
-from legopolitics.storage.export import export_result, load_result, result_tables
-from legopolitics.storage.manifest import RunManifest
-from legopolitics.storage.paths import OutputLayout
-
-__all__ = ["OutputLayout", "RunManifest", "export_result", "load_result", "result_tables"]
