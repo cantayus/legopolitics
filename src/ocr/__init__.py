699f6e2edd0949d51d73bc8d5528186d1396133e
cantayus <cantay.caliskan@gmail.com>
2026-07-13 13:27:10 +0300

    Delete src/ocr directory

--- src/ocr/__init__.py
+++
@@ -1,5 +0,0 @@
-from legopolitics.ocr.deduplicate import deduplicate_temporally
-from legopolitics.ocr.florence import FlorenceOCRAdapter
-from legopolitics.ocr.trocr import TrOCRAdapter
-
-__all__ = ["FlorenceOCRAdapter", "TrOCRAdapter", "deduplicate_temporally"]
