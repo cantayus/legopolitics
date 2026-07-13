b974b4adc738b0d5301815cb2648f9bd6fc8fa58
cantayus <cantay.caliskan@gmail.com>
2026-07-13 13:57:09 +0300

    Delete vision directory

--- vision/prompts.py
+++
@@ -1,3 +0,0 @@
-from legopolitics.prompting.templates import BLIND_OBSERVATION_TEMPLATE, CONTEXTUAL_TEMPLATE
-
-__all__ = ["BLIND_OBSERVATION_TEMPLATE", "CONTEXTUAL_TEMPLATE"]
--- vision/registry.py
+++
@@ -1,3 +0,0 @@
-from legopolitics.detection.registry import AdapterRegistry
-
-VISION_MODELS = AdapterRegistry("legopolitics.vision_models")
